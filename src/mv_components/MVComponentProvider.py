118de291edac0dd78f468da42b6182ff9ef6b37c
Jarriq Rolle <jrolle@equitybahamas.com>
2022-10-18 00:47:09 -0400

    Added optional sidecar flag to mv-component:make
    This adds the ability to create a component within its own folder.

--- src/mv_components/MVComponentProvider.py
+++ src/mv_components/MVComponentProvider.py
@@ -26,30 +26,54 @@ class CreateComponentCommand(Command):
 
     mv-component:make
         {name : Name of component}
-        {--f|flag : An optional argument for the command}
+        {--s|sidecar : Should this be placed in a subdirectory}
         {--o|option=default: An optional argument for the command with default value}
     """
 
     def handle(self):
         name = self.argument('name')
+        sidecar = self.option('sidecar')
+
         COMPONENTS_DIR = config_loader()
-        """templates folder must be root directory of components path."""
-        file_name_py = f"{camelize(name)}.py"
-        file_name_html = f"{camelize(name)}.html"
-        full_directory_path = os.path.join(os.getcwd() + '\\templates', COMPONENTS_DIR)
+        if not sidecar:
+            """templates folder must be root directory of components path."""
+            file_name_py = f"{camelize(name)}.py"
+            file_name_html = f"{camelize(name)}.html"
+            full_directory_path = os.path.join(os.getcwd() + '\\templates', COMPONENTS_DIR)
 
-        if os.path.exists(os.path.join(full_directory_path, file_name_py)) or os.path.exists(os.path.join(full_directory_path, file_name_html)):
-            self.line(
-                f'<error>Component "{name}" Already Exists ({full_directory_path}\\{file_name_py} or {file_name_html})</error>'
-            )
-            return
+            if os.path.exists(os.path.join(full_directory_path, file_name_py)) or os.path.exists(os.path.join(full_directory_path, file_name_html)):
+                self.line(
+                    f'<error>Component "{name}" Already Exists ({full_directory_path}\\{file_name_py} or {file_name_html})</error>'
+                )
+                return
 
-        stub = str(pathlib.Path(__file__).parent.absolute()) + r"\stubs\component.stub"
-        f = open(stub).read()
-        component = f.replace("__CLASS__", camelize(name))
-        f = open(os.path.join(full_directory_path, file_name_py), 'w').write(component)
-        f = open(os.path.join(full_directory_path, file_name_html), 'w').write("")
-        self.info(f"Component created: {name}")
+            stub = str(pathlib.Path(__file__).parent.absolute()) + r"\stubs\component.stub"
+            f = open(stub).read()
+            component = f.replace("__CLASS__", camelize(name))
+            f = open(os.path.join(full_directory_path, file_name_py), 'w').write(component)
+            f = open(os.path.join(full_directory_path, file_name_html), 'w').write("")
+            self.info(f"Component created: {name}")
+        else:
+            """templates folder must be root directory of components path."""
+            file_name_py = f"{camelize(name)}.py"
+            file_name_html = f"{camelize(name)}.html"
+            full_directory_path = os.path.join(os.getcwd() + '\\templates', COMPONENTS_DIR + f'\\{camelize(name)}')
+            print(full_directory_path)
+            if os.path.exists(os.path.join(full_directory_path, file_name_py)) or os.path.exists(
+                    os.path.join(full_directory_path, file_name_html)):
+                self.line(
+                    f'<error>Component "{name}" Already Exists ({full_directory_path}\\{file_name_py} or {file_name_html})</error>'
+                )
+                return
+
+            stub = str(pathlib.Path(__file__).parent.absolute()) + r"\stubs\component.stub"
+            f = open(stub).read()
+            component = f.replace("__CLASS__", camelize(name))
+            #print(os.path.join(full_directory_path, file_name_py))
+            os.makedirs(full_directory_path)
+            f = open(os.path.join(full_directory_path, file_name_py), 'w').write(component)
+            f = open(os.path.join(full_directory_path, file_name_html), 'w').write("")
+            self.info(f"Component created: {name}")
 
 
 class MVComponentProvider(Provider):
